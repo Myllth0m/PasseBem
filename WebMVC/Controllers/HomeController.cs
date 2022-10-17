845ff924fc40e74e742b14d0b3fac1c18badb75f
Milton D Yama <miltondyama@gmail.com>
2022-10-17 17:52:49 -0400

    criei a consulta de dias semanais para cidade selecionada no dropdown

--- WebMVC/Controllers/HomeController.cs
+++ WebMVC/Controllers/HomeController.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
-using FakerDotNet;
 using WebMVC.Data;
 using WebMVC.Models;
 
@@ -14,6 +13,7 @@ namespace WebMVC.Controllers
         {
             var cidadesFrias = new List<CidadeViewModel>();
             var cidadesQuentes = new List<CidadeViewModel>();
+            var todasAsCidades = new List<Cidade>();
 
             using (var contexto = new PasseBemContexto())
             {
@@ -21,6 +21,7 @@ namespace WebMVC.Controllers
                 contexto.Configuration.ProxyCreationEnabled = false;
 
                 cidadesFrias = contexto.PrevisaoClima
+                    .AsNoTracking()
                     .Include("Cidade")
                     .Include("Cidade.Estado")
                     .OrderBy(x => x.TemperaturaMinima)
@@ -37,6 +38,7 @@ namespace WebMVC.Controllers
                     .ToList();
 
                 cidadesQuentes = contexto.PrevisaoClima
+                    .AsNoTracking()
                     .Include("Cidade")
                     .Include("Cidade.Estado")
                     .OrderByDescending(x => x.TemperaturaMinima)
@@ -52,13 +54,20 @@ namespace WebMVC.Controllers
                     })
                     .ToList();
 
+                todasAsCidades = contexto.Cidade
+                    .AsNoTracking()
+                    .ToList();
+
+
+
                 contexto.Configuration.LazyLoadingEnabled = true;
                 contexto.Configuration.ProxyCreationEnabled = true;
             }
 
             ViewBag.CidadesFrias = cidadesFrias;
             ViewBag.CidadesQuentes = cidadesQuentes;
-            
+            ViewBag.SelectListDetodasAsCidades = todasAsCidades;
+
             return View();
         }
 
@@ -66,41 +75,39 @@ namespace WebMVC.Controllers
         {
             ViewBag.Message = "Your application description page.";
 
-            //using (var contexto = new PasseBemContexto())
-            //{
-            //    var listaDePrevisoes = new List<PrevisaoClima>();
-
-            //    for (int i = 1; i < 9; i++)
-            //    {
-            //        for (int j = 0; j < 9; j++)
-            //        {
-            //            var clima = Faker.Random.Element(new string[] { "instavel", "tempestuoso", "chuvoso", "nublado", "ensolarado" });
-            //            var tempMin = Faker.Random.Element(new decimal[] { 8m, 9m, 10m, 11m, 12m, 13m, 14m, 15m, 16m, 17m, 18m, 19m, 20m, 21m, 22m, 23m, 24m, 25m, 26m, 27m, 28m, 29m });
-            //            var tempMax = Faker.Random.Element(new decimal[] { 29m, 30m, 31m, 32m, 33m, 34m, 35m, 36m, 37m, 38m, 39m, 40m, 41m });
-
-            //            listaDePrevisoes.Add(new PrevisaoClima()
-            //            {
-            //                CidadeId = i,
-            //                Clima = clima,
-            //                DataPrevisao = DateTime.Now.AddDays(j),
-            //                TemperaturaMinima = tempMin,
-            //                TemperaturaMaxima = tempMax
-            //            });
-            //        }
-            //    }
-
-            //    contexto.PrevisaoClima.AddRange(listaDePrevisoes);
-            //    contexto.SaveChanges();
-            //}
-
             return View();
         }
 
-        public ActionResult Contact()
+        public PartialViewResult ObterClimaSemanal(int idDaCidadeSelecionada)
         {
-            ViewBag.Message = "Your contact page.";
+            var climaSemanalParaCidadeSelecionada = new List<ClimaSemanalParaCidadeSelecionadaViewModel>();
 
-            return View();
+            using (var contexto = new PasseBemContexto())
+            {
+                contexto.Configuration.LazyLoadingEnabled = false;
+                contexto.Configuration.ProxyCreationEnabled = false;
+
+                climaSemanalParaCidadeSelecionada = contexto.PrevisaoClima
+                    .AsNoTracking()
+                    .Include("Cidade")
+                    .OrderBy(x => x.DataPrevisao)
+                    .Where(x => x.CidadeId == idDaCidadeSelecionada)
+                    .Take(7)
+                    .Select(x => new ClimaSemanalParaCidadeSelecionadaViewModel()
+                    {
+                        NomeDaCidade = x.Cidade.Nome,
+                        DiaDaSemana = x.DataPrevisao.ToString(),
+                        TempoClimatico = x.Clima,
+                        TemperaturaMinima = x.TemperaturaMinima.Value,
+                        TemperaturaMaxima = x.TemperaturaMaxima.Value
+                    })
+                    .ToList();
+
+                contexto.Configuration.LazyLoadingEnabled = true;
+                contexto.Configuration.ProxyCreationEnabled = true;
+            }
+
+            return PartialView("_ClimaSemanalParaCidadeSelecionada", climaSemanalParaCidadeSelecionada);
         }
     }
 }
---
+++ WebMVC/Models/ClimaSemanalParaCidadeSelecionadaViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace WebMVC.Models
+{
+    public class ClimaSemanalParaCidadeSelecionadaViewModel
+    {
+        public string NomeDaCidade { get; set; }
+        public string DiaDaSemana { get; set; }
+        public string TempoClimatico { get; set; }
+        public decimal TemperaturaMinima { get; set; }
+        public decimal TemperaturaMaxima { get; set; }
+    }
+}
